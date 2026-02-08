869f44958b61241f1e8d83791e7fab4d1f8f95ed
Vimarsh Sathia <vsathia2@illinois.edu>
2026-02-08 14:16:17 -0600

    Do correct rejection sampling without going into infinite loop

--- gen_pysides_from_original.jl
+++ gen_pysides_from_original.jl
@@ -1,7 +1,11 @@
 ENV["JULIA_DEBUG"] = "Reactant,Reactant_jll"
 using Reactant
+using DataFrames
+using Random
 Reactant.set_default_backend("cpu")
-Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
+Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = false
+const BENCHMARK_SEED = 1234
+Random.seed!(BENCHMARK_SEED)
 
 include("load_params.jl")
 include("load_sides_csv.jl")
@@ -9,16 +13,14 @@ include("gen_sfr_props.jl")
 include("gen_magnification.jl")
 include("gen_fluxes.jl")
 
-
+## Initialize stuff
 csv_idl_path = "data/SIDES_Bethermin2017_short.csv"
-
 params = load_params("SIDES_from_original.par")
-
-cat = load_sides_csv(csv_idl_path)
-
+cat_ra = load_sides_csv(csv_idl_path)
 sfr_params = parse_sfr_params(params)
 
-function gen_sfr_props2(cat, p)
+function gen_sfr_props2(cat::DataFrame, p::NamedTuple)
+    # Read parameters
     (; Chab2Salp_num, Mt0, alpha1, alpha2, sigma0, beta1, beta2, qfrac0, gamma,
         m1, a2, m0, a0, a1, corr_zmean_lowzcorr, zmax_lowzcorr, zmean_lowzcorr,
         Psb_hz, slope_Psb, z_Psb_knee, sigma_MS, logx0, logBsb, SFR_max) = p
@@ -34,7 +36,6 @@ function gen_sfr_props2(cat, p)
     Prob_SF = (1.0 .- qfrac0z) .* 0.5 .* (1.0 .- erf.((log10.(cat.Mstar) .- Mtz) ./ sigmaz))
     Xuni = rand(Ngal)
     qflag = Xuni .> Prob_SF
-
 
     m_all = log10.(cat.Mstar .* Chab2Salp_num ./ 1.0e9)
     r_all = log10.(1.0 .+ cat.redshift)
@@ -54,21 +55,29 @@ function gen_sfr_props2(cat, p)
     # Use parsed parameters here
     issb_term_all = issb_all .* (logBsb - logx0)
     SFR_all = 10.0 .^ (logSFRms_all .+ sigma_MS .* noise_all .+ logx0 .+ issb_term_all)
-
     mask_SF = .!qflag
-    println("Deal with SFR drawn initially above the SFR limit...")
-    too_high = (SFR_all .> SFR_max) .& mask_SF
     
-    @trace track_numbers=false while any(too_high)
-        redraw_noise = randn(Ngal)
-        redraw = 10.0 .^ (logSFRms_all .+ sigma_MS .* redraw_noise .+ logx0 .+ issb_term_all)
-        SFR_all = ifelse.(too_high, redraw, SFR_all)
-        too_high = (SFR_all .> SFR_max) .& mask_SF
-    end
+    println("Deal with SFR drawn initially above the SFR limit...")
+    too_high_mask = (SFR_all .> SFR_max) .& mask_SF
 
+    # Redraw only the initially invalid SF entries, one index at a time.
+    @trace track_numbers=false for i in 1:Ngal
+        needs_redraw_i = @allowscalar too_high_mask[i]
+        @trace track_numbers=false while needs_redraw_i
+            logSFRms_i = @allowscalar logSFRms_all[i]
+            issb_i = @allowscalar issb_all[i]
+            redraw_noise_i = randn()
+            issb_term_i = issb_i * (logBsb - logx0)
+            redraw_i = 10.0 ^ (logSFRms_i + sigma_MS * redraw_noise_i + logx0 + issb_term_i)
+            @allowscalar setindex!(SFR_all, redraw_i, i)
+            needs_redraw_i = redraw_i > SFR_max
+        end
+    end
+    
+    println("Finished loop computation")
     SFR_final = ifelse.(mask_SF, SFR_all, zero(eltype(SFR_all)))
     issb_final = ifelse.(mask_SF, issb_all, false)
-
+    
     return (qflag, SFR_final, issb_final)
 end
 
@@ -79,19 +88,19 @@ function process(cat)
 	# cat = gen_fluxes(cat, params)
 end
 
-cat_ra = (
-    redshift = Reactant.ConcreteRArray(cat.redshift),
-    ra = Reactant.ConcreteRArray(cat.ra),
-    dec = Reactant.ConcreteRArray(cat.dec),
-    Mhalo = Reactant.ConcreteRArray(cat.Mhalo),
-    Mstar = Reactant.ConcreteRArray(cat.Mstar),
-)
+# cat_ra = DataFrame(
+#     redshift = Reactant.ConcreteRArray(cat.redshift),
+#     ra = Reactant.ConcreteRArray(cat.ra),
+#     dec = Reactant.ConcreteRArray(cat.dec),
+#     Mhalo = Reactant.ConcreteRArray(cat.Mhalo),
+#     Mstar = Reactant.ConcreteRArray(cat.Mstar),
+# )
 (qflag, SFR, issb) = @jit process(cat_ra)
+println("\n=== Finished Computations ===")
 
-println("\n=== Results ===")
-println("qflag: ", typeof(qflag), " size=", size(qflag))
-println("  quenched count: ", sum(Array(qflag)))
-println("SFR: ", typeof(SFR), " size=", size(SFR))
-println("  min=", minimum(Array(SFR)), " max=", maximum(Array(SFR)), " mean=", sum(Array(SFR))/length(SFR))
-println("issb: ", typeof(issb), " size=", size(issb))
-println("  starburst count: ", sum(Array(issb)))
+# println("qflag: ", typeof(qflag), " size=", size(qflag))
+# println("  quenched count: ", sum(ifelse.(qflag, 1, 0)))
+# println("SFR: ", typeof(SFR), " size=", size(SFR))
+# println("  min=", minimum(SFR), " max=", maximum(SFR), " mean=", sum(SFR) / length(SFR))
+# println("issb: ", typeof(issb), " size=", size(issb))
+# println("  starburst count: ", sum(ifelse.(issb, 1, 0)))
