9557d1f05607e5e3a97932fca7f91f1ec384407d
Kedar A Phadke <40395196+phadkekd@users.noreply.github.com>
2026-02-26 15:29:10 -0600

    Added files for filters
    tests julia implementation and computation of fluxes in filters

---
+++ gen_pysides_from_original_julia_test.jl
@@ -0,0 +1,21 @@
+include("load_params.jl")
+include("load_sides_csv.jl")
+include("gen_sfr_props.jl")
+include("gen_magnification.jl")
+include("gen_fluxes.jl")
+include("gen_fluxes_filter.jl")
+
+
+csv_idl_path = "/Volumes/T7 Shield/SIDES/test/SIDES_Bethermin2017_short.csv"
+
+params = load_params("SIDES_from_original.par")
+
+cat = load_sides_csv(csv_idl_path)
+
+cat = gen_sfr_props(cat, params)
+
+cat = gen_magnification(cat, params)
+
+cat = gen_fluxes(cat, params)
+
+cat = gen_fluxes_filter(cat, params)
